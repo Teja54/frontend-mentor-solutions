0c263cd8340e6715f5caddf356954553d7c0b3c9
Teja54 <tejadevisetti14@gmail.com>
2021-06-26 04:50:50 +0530

    Challenges

---
+++ js/script.js
@@ -0,0 +1,176 @@
+const projects = [
+  {
+    id: 1,
+    title: `Profile Card Component`,
+    img: "Design/profile-card-component-master.jpg",
+    site: "https://profile-card-component-teja54.vercel.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/Profile%20Card%20Component",
+  },
+  {
+    id: 2,
+    title: "Social Proof section",
+    img: "Design/social-proof-section-master.png",
+    site: "https://social-proof-section-master-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/social-proof-section-master",
+  },
+  {
+    id: 3,
+    title: "article preview component",
+    img: "Design/article-preview-component-master.png",
+    site: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/article-preview-component-master",
+    github: "https://article-preview-component-master-teja54.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "coding bootcamp testimonial slider",
+    img: "Design/coding-bootcamp-testimonials-slider-master.png",
+    site: "https://coding-bootcamp-testimonials-slider-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/coding-bootcamp-testimonials-slider-master",
+  },
+  {
+    id: 5,
+    title: "four card feature section ",
+    img: "Design/four-card-feature-section-master.png",
+    site: "https://four-card-feature-section-master-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/four-card-feature-section-master",
+  },
+  {
+    id: 6,
+    title: "FAQ Accordion card",
+    img: "Design/FAQ-accordation-card.png",
+    site: "https://faq-accordion-card-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/FAQ%20accordation%20card",
+  },
+  {
+    id: 7,
+    title: "Base appareal coming soon",
+    img: "Design/base-apparel-coming-soon-master.png",
+    site: "https://base-apparel-coming-soon-master-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/base-apparel-coming-soon-master",
+  },
+  {
+    id: 8,
+    title: "Intro component with signup form",
+    img: "Design/intro-component-with-signup-form.png",
+    site: "https://intro-component-with-signup-form-master-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/intro-component-with-signup-form-master",
+  },
+  {
+    id: 9,
+    title: "single price grid component",
+    img: "Design/single-price-grid-component.png",
+    site: "https://single-price-grid-component-master-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/single-price-grid-component-master",
+  },
+  {
+    id: 10,
+    title: "Ping Coming soon page",
+    img: "Design/ping-coming-soon-page.png",
+    site: "https://ping-coming-soon-page-master-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/ping-coming-soon-page-master",
+  },
+  {
+    id: 11,
+    title: "huddle landing page with single introductory section",
+    img: "Design/huddle-landing-page-with-single-introductory-section.png",
+    site: "https://huddle-landing-page-with-single-introductory-section-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/huddle-landing-page-with-single-introductory-section-master",
+  },
+  {
+    id: 12,
+    title: "3 Column preview card",
+    img: "Design/3-column-preview-card-component-main.png",
+    site: "https://3-column-preview-card-component-main-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/3-column-preview-card-component-main",
+  },
+  {
+    id: 13,
+    title: "Testimonial Grid Section",
+    img: "Design/testimonials-grid-section-main.png",
+    site: "https://testimonials-grid-section-main-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/testimonials-grid-section-main",
+  },
+  {
+    id: 14,
+    title: "Fylo Data Storage Component",
+    img: "Design/fylo-data-storage-component-master.png",
+    site: "https://fylo-data-storage-component-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/fylo-data-storage-component-master",
+  },
+  {
+    id: 15,
+    title: "clipboard landing page",
+    img: "Design/clipboard-landing-page-master.png",
+    site: "https://clipboard-landing-page-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/clipboard-landing-page-master",
+  },
+  {
+    id: 16,
+    title: "Huddle landing page with curved sections",
+    img: "Design/huddle-landing-page-with-curved-sections-master.png",
+    site: "https://huddle-landing-page-with-curved-sections-master.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/huddle-landing-page-with-curved-sections-master",
+  },
+  {
+    id: 17,
+    title: "Huddle Landing page with alternating feature blocks",
+    img: "Design/huddle-landing-page-with-alternating-feature-blocks-master.png",
+    site: "https://huddle-landing-page-with-alternating-feature-blocks-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/huddle-landing-page-with-curved-sections-master",
+  },
+  {
+    id: 18,
+    title: "Fylo landing page with two column layout",
+    img: "Design/fylo-landing-page-with-two-column-layout.png",
+    site: "https://fylo-landing-page-with-two-column-layout-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/fylo-landing-page-with-two-column-layout-master",
+  },
+  {
+    id: 19,
+    title: "Project tracking intro component",
+    img: "Design/project-tracking-intro-component-master.png",
+    site: "https://project-tracking-intro-component-master-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/project-tracking-intro-component-master",
+  },
+  {
+    id: 20,
+    title: "Fylo dark landing page",
+    img: "Design/fylo-dark-theme-landing-page.png",
+    site: "https://fylo-dark-theme-landing-page-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/fylo-dark-theme-landing-page-master",
+  },
+  {
+    id: 21,
+    title: "Blogr landing page",
+    img: "Design/blogr-landing-page.png",
+    site: "https://blogr-landing-page-teja54.netlify.app/",
+    github: "https://github.com/Teja54/frontend-mentor-challenges/tree/master/blogr-landing-page-main",
+  },
+];
+
+const list = document.getElementById('list');
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  displayPageItems(projects);
+});
+
+function displayPageItems(items) {
+  let page = items.map(function(item) {
+    return `<li>
+    <a href="${item.site}" target="_blank">
+      <img src="${item.img}" alt="" />
+      <p>${item.id}. ${item.title}</p>
+    </a>
+    <div class="links-container">
+      <a href="${item.site}" target="_blank" class="preview">
+        <i class="fas fa-eye"></i>
+      </a>
+      <a href="${item.github}" target="_blank" class="git-link">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+  </li>`;
+  }).join(' ');
+  list.innerHTML = page;
+  console.log(page);
+};
